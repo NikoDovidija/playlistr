98b585de8d0af4d2dd44e8475860f17910529934
Jost Gombac <gombac.jost@gmail.com>
2018-03-31 15:44:32 +0200

    basic structure, styles, components,

---
+++ resources/assets/js/components/header/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <div class="width-limiter">
+            <div class="grid">
+                <div class="logo grid-item">laylistR</div>
+                <menu-component></menu-component>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    import MenuComponent from '../header/Menu.vue';
+
+    export default {
+        components: {
+            "menu-component": MenuComponent
+        },
+        data () {
+            return {};
+        },
+    };
+</script>
+
