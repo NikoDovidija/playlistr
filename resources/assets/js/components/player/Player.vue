01057e2cfdaa39c40aeea7ee4fd4def77046b521
Jost Gombac <gombac.jost@gmail.com>
2018-04-01 13:18:46 +0200

    [player] playtime manipulation

--- resources/assets/js/components/player/Player.vue
+++ resources/assets/js/components/player/Player.vue
@@ -2,20 +2,26 @@
     <div id="player" class="player">
         {{ id }}
         <div class="playtime">
-            <div class="bar"></div>
-            <div class="bar current" v-bind:style="{ width: (playtimeRelative * 100) + '%', }"></div>
-            <div class="bar-cursor" v-bind:style="{ right: ((1 - playtimeRelative) * 100) + '%' }"></div>
+            <div class="bar"
+                ref="bar"
+                @click="setPlaytime"></div>
+            <div class="bar current" 
+                v-bind:style="{ width: (playtimeRelative * 100) + '%', }"
+                @click="setPlaytime"></div>
+            <div class="bar-cursor" 
+                v-bind:style="{ right: ((1 - playtimeRelative) * 100) + '%' }"
+                ></div>
         </div>
         <div class="controls">
 
             <span class="control" 
                     v-bind:class="{ hide: isPlaying }"
-                    v-on:click="isPlaying = !isPlaying">
+                    v-on:click="play">
                 <i class="material-icons">play_arrow</i>
             </span>
             <span class="control" 
                     v-bind:class="{ hide: !isPlaying }"
-                    v-on:click="isPlaying = !isPlaying">
+                    v-on:click="pause">
                 <i class="material-icons">pause</i>
             </span>
             <span class="control">
@@ -37,16 +43,8 @@
                 playtimeRelative: 0,
             }
         },
-        watch: {
-            "playtime": function (val, old) {
-                console.log(val, old);
-            }
-        },
         created () {
-            let self = this;
-            setInterval(function () {
-                self.tickPlaytime();
-            }, 200);
+
         },
         methods: {
             setSong (id) {
@@ -54,9 +52,42 @@
                 this.id = id;
             },
 
+            play () {
+                this.isPlaying = true;
+                this.tickPlaytime();
+            },
+
+            pause () {
+                this.isPlaying = false;
+            },
+
+            setPlaytime (event) {
+                let bar = this.$refs.bar.getBoundingClientRect(),
+                    start = bar.left,
+                    width = bar.width,
+                    x = event.clientX - start;
+                let relative = x / width;
+                this.playtime = this.duration * relative;
+            },
+
+            songCompleted () {
+                this.isPlaying = false;
+                this.playtime = 0;
+            },
+
             tickPlaytime () {
+                let self = this;
+                if (!this.isPlaying) {
+                    return;
+                }
                 this.playtime = this.playtime + 0.2;
                 this.playtimeRelative = this.playtime / this.duration;
+                if (this.playtime >= this.duration) {
+                    this.songCompleted();
+                }
+                setTimeout(function () {
+                    self.tickPlaytime();
+                }, 200);
             }
         }
     }
